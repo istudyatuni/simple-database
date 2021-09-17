623cd69f2dfb5d370aa1515bf7c6cab081859e04
Ilia <istudyatuni@gmail.com>
2021-09-17 12:15:10 +0300

    Add read/write with cstdio

---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+
+struct rec {
+	int id;
+	char name[50];
+};
+
+void input(rec &d) {
+	printf("id: ");
+	scanf("%d", &d.id);
+
+	if (d.id <= 0) {
+		return;
+	}
+
+	printf("name: ");
+	scanf("%s", d.name);
+}
+
+void print(const rec &d) {
+	printf("%d, %s\n", d.id, d.name);
+}
+
+int main()
+{
+	rec s;
+	FILE *f;
+	if((f = fopen("db.bin", "ab")) != NULL) {
+		input(s);
+		while(s.id > 0) {
+		    fwrite(&s, sizeof(s), 1, f);
+			input(s);
+		}
+		fclose(f);
+	}
+
+	if((f = fopen("db.bin", "rb")) != NULL) {
+		fread(&s, sizeof(s), 1, f);
+		while(!feof(f)) {
+			print(s);
+			fread(&s, sizeof(s), 1, f);
+		}
+	}
+
+	return 0;
+}
