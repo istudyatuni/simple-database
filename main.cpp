d1c2069c1fca62d3076f8368fab712fd7e9866a0
Ilia <istudyatuni@gmail.com>
2021-10-08 10:15:22 +0300

    Remove commented code

--- main.cpp
+++ main.cpp
@@ -21,24 +21,5 @@ int main()
 	db.Find(7, s);
 	print(s);
 
-	// FILE *f, *fcopy;
-	// if((f = fopen("db.bin", "ab")) != NULL) {
-	// 	input(s);
-	// 	while(s.id > 0) {
-	// 		write(s, f);
-	// 		input(s);
-	// 	}
-	// 	fclose(f);
-	// }
-
-	// if((f = fopen("db.bin", "rb")) != NULL && (fcopy = fopen("dbcopy.bin", "ab")) != NULL) {
-	// 	read(s, f);
-	// 	while(!feof(f)) {
-	// 		print(s);
-	// 		// write(s, fcopy);
-	// 		read(s, f);
-	// 	}
-	// }
-
 	return 0;
 }
